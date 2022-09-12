7651c28587abfc461bb269077a31e26d9d24fa8c
Jair montenegro <jairmontenegrof@gmail.com>
2022-09-12 15:59:56 -0300

    adding active

--- src/App.js
+++ src/App.js
@@ -1,69 +1,59 @@
-import './App.css';
-import Button from './components/button.js';
-import Display from './components/display.js';
-import ButtonCLear from './components/clearButton.js';
-import {useState} from 'react';
-import {evaluate} from 'mathjs';
-import Footer from './components/footer.js';
+import "./App.css";
+import Button from "./components/button.js";
+import Display from "./components/display.js";
+import ButtonCLear from "./components/clearButton.js";
+import { useState } from "react";
+import { evaluate } from "mathjs";
+import Footer from "./components/footer.js";
 
 function App() {
+  const [input, setInput] = useState("");
 
-  const [input , setInput] = useState('');
-
-  const addInput = val =>{
-    setInput(input + val)
+  const addInput = (val) => {
+    setInput(input + val);
   };
 
   const calculate = () => {
-    if (input) {
-    setInput(evaluate(input))
-    }else {
-      alert('please insert values to calculate')
+    if (input) {
+      setInput(evaluate(input));
+    } else {
+      alert("please insert values to calculate");
     }
-  }
-
+  };
 
   return (
-      <div className='App'>
-        <div className='pincipal_container'>
-          <div className='calculator'>
-          
-            <Display input={input}/>
-            <ButtonCLear mClear={()=> setInput('')}>
-              C
-            </ButtonCLear>
-            <Button mClick={addInput}>DEL</Button>
-            <Button mClick={addInput}>%</Button>
-            <Button mClick={addInput}>/</Button>
-            <Button mClick={addInput}>7</Button>
-            <Button mClick={addInput}>8</Button>
-            <Button mClick={addInput}>9</Button>
-            <Button mClick={addInput}>*</Button>
-            <Button mClick={addInput}>4</Button>
-            <Button mClick={addInput}>5</Button>
-            <Button mClick={addInput}>6</Button>
-            <Button mClick={addInput}>-</Button>
-            <Button mClick={addInput}>1</Button>
-            <Button mClick={addInput}>2</Button>
-            <Button mClick={addInput}>3</Button>
-            <Button mClick={addInput}>+</Button>
-            <Button mClick={addInput}>0</Button>
-            <Button mClick={addInput}>.</Button>
-            <Button mClick={calculate}>=</Button>
+    <div className="App">
+      <div className="pincipal_container">
+        <div className="calculator">
+          <Display input={input} />
+          <ButtonCLear mClear={() => setInput("")}>C</ButtonCLear>
+          <Button mClick={addInput}>DEL</Button>
+          <Button mClick={addInput}>%</Button>
+          <Button mClick={addInput}>/</Button>
+          <Button mClick={addInput}>7</Button>
+          <Button mClick={addInput}>8</Button>
+          <Button mClick={addInput}>9</Button>
+          <Button mClick={addInput}>*</Button>
+          <Button mClick={addInput}>4</Button>
+          <Button mClick={addInput}>5</Button>
+          <Button mClick={addInput}>6</Button>
+          <Button mClick={addInput}>-</Button>
+          <Button mClick={addInput}>1</Button>
+          <Button mClick={addInput}>2</Button>
+          <Button mClick={addInput}>3</Button>
+          <Button mClick={addInput}>+</Button>
+          <Button mClick={addInput}>0</Button>
+          <Button mClick={addInput}>.</Button>
+          <Button mClick={calculate}>=</Button>
         </div>
-
-        </div>
-        <Footer/>
       </div>
-
-    
-  )
+      <Footer />
+    </div>
+  );
 }
-
 
 export default App;
 
-
-// arrow function must have ;. 
-// add second display 
+// arrow function must have ;.
+// add second display
 // add same funtionalitys from porject js
